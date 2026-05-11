e71d36f3ea2b3622a97b9fa1c5073f773868e539
Sasith Isuru <87513044+Dark-str@users.noreply.github.com>
2026-05-11 23:12:10 +0530

    Revise multi-line comment in Q1.py
    Updated multi-line comment to clarify its purpose.

--- PF/Q1.py
+++ PF/Q1.py
@@ -4,5 +4,9 @@ print("Sasith Isuru\n Thurstan college")
 #The above text shows the name of the student and his school
 
 """
-This is a multi line comment
+This is a multi line comment.
+
+Why we use comments in python?
+1.They act as a guide for others. So they can clearly understand the code and the purpose(intention) of it.
+2.Ease of debugging.
 """
